1edf90f3ce8ca7b6b1688f8403b745c48b15fc5d
jerzha40 <jerryzhang40@gmail.com>
2025-11-08 17:31:27 +0800

    test

--- code/test.py
+++ code/test.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 from utilsarma.core import DataProcessor
 from pathlib import Path
 
@@ -19,3 +20,39 @@ logging.basicConfig(
 DP = DataProcessor(Path(__file__).parent.parent / "data" / "BTCUSDT_1d.parquet")
 DP.load_data()
 logging.info(f"{DP.table}")
+logging.info(f"{DP.table['Close']}")
+logging.info(f"{type(DP.table['Close'])}")
+
+close_ca = DP.table["Close"].combine_chunks()  # ChunkedArray -> Array(ListType)
+close_list = close_ca.to_pylist()
+close = np.asarray(close_list, dtype=np.float64)  # 价格字符串 -> float64
+
+open_time_ca = DP.table["Open Time"].combine_chunks()  # 同理
+open_time_list = open_time_ca.to_pylist()  # -> Python list of int(ms)
+time = np.asarray(open_time_list, dtype="datetime64[ms]")  # 毫秒 -> datetime64[ms]
+
+logging.info(f"{open_time_ca}")
+logging.info(f"{open_time_list}")
+logging.info(f"{time}")
+logging.info(f"{len(close)}")
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+fig, ax = plt.subplots(figsize=(10, 4))
+ax.plot(time, close, ".-")
+ax.set_title("BTC/USDT Close")
+ax.set_xlabel("Time (UTC)")
+ax.set_ylabel("Price (USDT)")
+ax.grid(True, which="major", linestyle="--", linewidth=0.6, alpha=0.7)  # 主网格
+ax.minorticks_on()  # 打开次刻度（更细的刻度线）
+ax.grid(
+    True, which="minor", linestyle=":", linewidth=0.4, alpha=0.5
+)  # 次网格ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+fig.tight_layout()
+output_path = Path(__file__).parent.parent / "report" / "figs"
+output_path.mkdir(parents=True, exist_ok=True)  # 确保目录存在
+fig.savefig(output_path / "close_np.png", dpi=150)
+plt.show()
+plt.close(fig)
--- reqs.bat
+++ reqs.bat
@@ -1,4 +1,6 @@
 python -m pip install pip --upgrade
 python -m pip install pyarrow pyarrow-stubs
+python -m pip install numpy
+python -m pip install matplotlib
 python -m pip install -e ./packages
 python -m pip freeze > reqs.txt
